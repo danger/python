3b69c12aefa361b106f150074a778caccda58c7c
Jakub Turek <jkbturek@gmail.com>
2020-01-15 16:48:34 +0100

    Use stubbed subprocess.run call instead of the real one

--- tests/test_shell.py
+++ tests/test_shell.py
@@ -4,14 +4,19 @@ from unittest import mock
 from danger_python.shell import resolve_danger_path
 
 
-def test_danger_path_can_be_resolved():
+@mock.patch('subprocess.run')
+def test_danger_path_can_be_resolved(mock_subprocess_run):
     """
     Test that danger-js path can be resolved.
     """
     def fake_run(commands, capture_output):
         if commands == ['which', 'danger'] and capture_output:
-            return subprocess.CompletedProcess(0, stdout='/usr/bin/fake-danger-js\n\n')
+            output = '/usr/bin/fake-danger-js \n \r'.encode('utf-8')
+            return subprocess.CompletedProcess(commands, 0, stdout=output)
+        else:
+            raise ValueError('Expected run to receive `which danger` command')
 
+    mock_subprocess_run.side_effect = fake_run
     resolved_path = resolve_danger_path()
 
-    assert resolved_path == "/Users/kuba/.nvm/versions/node/v10.6.0/bin/danger"
+    assert resolved_path == "/usr/bin/fake-danger-js"
