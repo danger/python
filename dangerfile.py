8e057bb354993916319d289ac93ba8c58ee14920
Jakub Turek <jkbturek@gmail.com>
2020-01-23 15:22:50 +0100

    Reimplement the dangerfile.py to match the brand new danger style

--- dangerfile.py
+++ dangerfile.py
@@ -1,20 +1,2 @@
-import json
-import sys
-
-# Read in DSL
-data = sys.stdin.read()
-dsl = json.loads(data)
-
-# Grabbing the DSL
-title = dsl["danger"]["github"]["pr"]["title"]
-
-# Create a results dict
-results = {
-    "warnings": [],
-    "messages": [],
-    "fails": [],
-    "markdowns": [{"message": title}],
-}
-
-# Print that to send a message back to danger
-print(json.dumps(results))
+title = danger.github.pr.title
+markdown(title)
