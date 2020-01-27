b95a17ee5445c4a5d2d2c22e497ba49c054d8c7e
Jakub Turek <jkbturek@gmail.com>
2020-01-27 11:51:05 +0100

    Add a rough implementation of resolving dependencies

--- danger_python/generator/builder.py
+++ danger_python/generator/builder.py
@@ -1,30 +1,56 @@
-from typing import List
+from typing import Any, Iterable, List, Optional
 
 from .models import (
     ClassDefinition,
     PropertyDefinition,
     SchemaItem,
     SchemaObject,
+    SchemaReference,
     SchemaValue,
 )
 
 
+def unzip(iterable: Iterable[Any]) -> Iterable[Any]:
+    return zip(*iterable)
+
+
 def build_classes(schema: List[SchemaObject]) -> List[ClassDefinition]:
-    return [_build_class(schema_object) for schema_object in schema]
+    built_classes = list(_build_class(schema_object) for schema_object in schema)
+    resolved = []
+    resolved_names = set()
+
+    while len(resolved) != len(built_classes):
+        unresolved = filter(lambda b: b[0].name not in resolved_names, built_classes)
+        for (_class, references) in unresolved:
+            if all(map(lambda r: r in resolved_names, references)):
+                resolved_names.add(_class.name)
+                resolved.append(_class)
+
+    return resolved
 
 
-def _build_class(object: SchemaObject) -> ClassDefinition:
-    return ClassDefinition(
-        name=object.name,
-        properties=[_build_property(property) for property in object.properties],
+def _build_class(object: SchemaObject) -> (ClassDefinition, List[str]):
+    properties, references = unzip(
+        (_build_property(property) for property in object.properties)
+    )
+
+    return (
+        ClassDefinition(name=object.name, properties=list(properties)),
+        list(filter(None, references)),
     )
 
 
-def _build_property(item: SchemaItem) -> PropertyDefinition:
+def _build_property(item: SchemaItem) -> (PropertyDefinition, Optional[str]):
     if isinstance(item, SchemaValue):
-        return _property_from_value(item)
+        return (_property_from_value(item), None)
+    if isinstance(item, SchemaReference):
+        return (_property_from_reference(item), item.reference)
 
 
 def _property_from_value(value: SchemaValue) -> PropertyDefinition:
-    mappings = {"string": "str", "boolean": "bool"}
+    mappings = {"string": "str", "boolean": "bool", "number": "int"}
     return PropertyDefinition(name=value.name, value_type=mappings[value.value_type])
+
+
+def _property_from_reference(reference: SchemaReference) -> PropertyDefinition:
+    return PropertyDefinition(name=reference.name, value_type=reference.reference)
--- tests/generator/test_builder.py
+++ tests/generator/test_builder.py
@@ -1,10 +1,7 @@
 from danger_python.generator.builder import build_classes
-from danger_python.generator.models import (
-    ClassDefinition,
-    PropertyDefinition,
-    SchemaObject,
-    SchemaValue,
-)
+from danger_python.generator.models import (ClassDefinition,
+                                            PropertyDefinition, SchemaObject,
+                                            SchemaReference, SchemaValue)
 
 
 def test_class_builder_builds_correct_model_for_simple_class():
@@ -30,4 +27,40 @@ def test_class_builder_builds_correct_model_for_simple_class():
             PropertyDefinition(name="string_value", value_type="str"),
             PropertyDefinition(name="boolean_value", value_type="bool"),
         ],
+    )
+
+
+def test_class_builder_handles_reference_types():
+    """
+    Test class builder handles reference types correctly.
+    """
+    schema = [
+        SchemaObject(
+            name="ObjectA",
+            properties=[SchemaReference(name="ref_b", reference="ObjectB")],
+        ),
+        SchemaObject(
+            name="ObjectB",
+            properties=[SchemaReference(name="ref_c", reference="ObjectC")],
+        ),
+        SchemaObject(
+            name="ObjectC",
+            properties=[SchemaValue(name="int_value", value_type="number")],
+        ),
+    ]
+
+    build_result = build_classes(schema)
+
+    assert len(build_result) == 3
+    assert build_result[0] == ClassDefinition(
+        name="ObjectC",
+        properties=[PropertyDefinition(name="int_value", value_type="int")],
+    )
+    assert build_result[1] == ClassDefinition(
+        name="ObjectB",
+        properties=[PropertyDefinition(name="ref_c", value_type="ObjectC")],
+    )
+    assert build_result[2] == ClassDefinition(
+        name="ObjectA",
+        properties=[PropertyDefinition(name="ref_b", value_type="ObjectB")],
     )
