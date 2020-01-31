ad2ca3a89e6664ca51b8b25645c0b9ea2f0ecc7e
Jakub Turek <jkbturek@gmail.com>
2020-01-31 11:22:32 +0100

    Inject danger DSL and output providing methods into DangerPlugin

--- danger_python/plugins.py
+++ danger_python/plugins.py
@@ -1,15 +1,32 @@
 import sys
+from typing import Optional
+
+from .danger import Danger, fail, markdown, message, warn
 
 
 class DangerPlugin:
+    def __init__(self):
+        self._danger = None
+        self.message = message
+        self.markdown = markdown
+        self.warn = warn
+        self.fail = fail
+
     def __init_subclass__(cls, **kwargs):
         parent_module = cls.__module__.split(".")[0]
         module = sys.modules[parent_module]
         instance = cls()
 
         for method_name in dir(cls):
-            if method_name.startswith("__"):
+            if method_name.startswith("__") or method_name in set(dir(DangerPlugin)):
                 continue
 
             method = getattr(instance, method_name)
             setattr(module, method_name, method)
+
+    @property
+    def danger(self):
+        if not self._danger:
+            self._danger = Danger()
+
+        return self._danger
--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,4 +1,7 @@
+import pytest
+
 import tests
+from danger_python.danger import Danger, DangerResults, Violation
 from danger_python.plugins import DangerPlugin
 
 
@@ -9,10 +12,41 @@ class FirstPlugin(DangerPlugin):
 
 def test_that_plugin_method_is_registered_in_global_namespace():
     """
-    Test that plguin method is autoregistered in global namespace of
+    Test that plugin method is autoregistered in global namespace of
     the package containing the method.
     """
 
     result = tests.fancy_method_returning_sum(8, 7)
 
     assert result == 15
+
+
+class ModifiedFilesPrinterPlugin(DangerPlugin):
+    def print_modified_files(self):
+        methods = [self.message, self.markdown, self.warn, self.fail]
+
+        for index, modified_file in enumerate(self.danger.git.modified_files):
+            number_of_files = len(self.danger.git.modified_files)
+            methods[index](f"Files modified: {number_of_files}", modified_file, index)
+
+
+@pytest.mark.parametrize(
+    "modified_files", [["message.py", "markdown.py", "warn.py", "fail.py"]]
+)
+def test_that_plugin_method_has_access_to_danger_and_reporting_methods(danger: Danger):
+    """
+    Test that plugin method has access to the danger DSL and also the methods
+    for providing messages, markdowns, warnings and fails.
+    """
+    tests.print_modified_files()
+
+    assert danger.results == DangerResults(
+        fails=[Violation(message="Files modified: 4", file_name="fail.py", line=3)],
+        warnings=[Violation(message="Files modified: 4", file_name="warn.py", line=2)],
+        markdowns=[
+            Violation(message="Files modified: 4", file_name="markdown.py", line=1)
+        ],
+        messages=[
+            Violation(message="Files modified: 4", file_name="message.py", line=0)
+        ],
+    )
